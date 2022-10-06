ca586b9590c4bdbd0fbd49e7f2fe7bc068bf5d10
HP <HP@192.168.1.19>
2022-10-05 21:27:16 -0500

    PRACTICA1

--- src/pactica/calificada/Ejercicio8.java
+++ src/pactica/calificada/Ejercicio8.java
@@ -4,6 +4,8 @@
  */
 package pactica.calificada;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP
@@ -13,8 +15,23 @@ public class Ejercicio8 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+    public static void main(String[]args) {
+ Scanner dato = new Scanner(System.in);
+ System.out.println("Ingrese cantidad de numeros ");
+ int elementos = dato.nextInt();
+ int[] n = new int[elementos];
+ System.out.println("Ingrese los números a ordenar ");
+ for(int a = 0; a < n.length; a++) {
+ n[a] = dato.nextInt();
+ }
+ int orden = 0; for(int b = 1; b < n.length; b++) {
+ for(int c = 0; c < n.length - b; c++) {
+ if(n[c] > n[c+1]) {
+ orden = n[c]; n[c] = n[c+1]; n[c+1] = orden;
+ } }
+ }
+ for(int i = 0; i <n.length; i++) {
+ System.out.print(n[i] + " ");
+ }
+ }
+ }
